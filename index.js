29a87d276a76c516de676443352c903c1c4f75d4
e17o4 <shirley.bookaholic@gmail.com>
2025-04-21 11:14:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+require('dotenv').config();
 const { Client, GatewayIntentBits } = require('discord.js');
+
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
 
 const quotes = [
   "Candy is delicious.",
@@ -58,13 +61,13 @@ const quotes = [
 ];
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Logged in as ${client.user.tag}`);
 });
 
-client.on('messageCreate', msg => {
-  if (msg.content === '!quote') {
+client.on('messageCreate', message => {
+  if (message.content === '!quote') {
     const quote = quotes[Math.floor(Math.random() * quotes.length)];
-    msg.channel.send(quote);
+    message.channel.send(quote);
   }
 });
 
